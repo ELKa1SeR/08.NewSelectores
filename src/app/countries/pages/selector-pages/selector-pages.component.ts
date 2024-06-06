caf81dbc2795bb5757f06935fd6b2c43e261b604
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-06 09:31:16 +0200

    "268"

--- src/app/countries/pages/selector-pages/selector-pages.component.ts
+++ src/app/countries/pages/selector-pages/selector-pages.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country';
 
 @Component({
   selector: 'app-selector-pages',
@@ -15,6 +17,13 @@ export class SelectorPagesComponent {
   })
 
     constructor(
-      private fb : FormBuilder
+      private fb : FormBuilder,
+      private countriesServices: CountriesService
     ){}
+
+    get regions(): Region[]{
+      return this.countriesServices.regions;
+    }
+
+    
 }
